2fed9e04525ac0b63b9c76c47b6d747c8b283c54
Harold Prentiss <harv.prentiss@gmail.com>
2014-03-12 20:17:10 -0400

    Adding description changes in about page.

--- PE Website/Javascript/aboutPage.js
+++ PE Website/Javascript/aboutPage.js
@@ -4,5 +4,14 @@ $(document).ready(function(){
 		obj.stopPropagation();
 	    $('a').removeClass('selected');
 	    $(this).addClass('selected');
+	    if($(this).attr('id') == "harry"){
+	    	$('.descriptionBox').html("<p>The only one who actually does any work.</p>");
+	    }
+	    else if($(this).attr('id') == "alex"){
+	    	$('.descriptionBox').html("<p>A tard who knows how to use pliers.</p>");
+	    }
+	    else if($(this).attr('id') == "eric"){
+	    	$('.descriptionBox').html('<p>An even bigger tard.</p>');
+	    }
 	});
 });
