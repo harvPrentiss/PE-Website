c558447cd2425101b61a43072dcdaa257c803526
Harold Prentiss <harv.prentiss@gmail.com>
2014-03-14 20:56:29 -0400

    More style adjustments
    The about page is now setup the way I want it to work. Need to animate
    the description box fading in and moving.

--- PE Website/Javascript/aboutPage.js
+++ PE Website/Javascript/aboutPage.js
@@ -3,7 +3,10 @@ $(document).ready(function(){
 		obj.preventDefault();
 		obj.stopPropagation();
 	    $('a').removeClass('selected');
+	    $('a').css('background-position', '0 0');
 	    $(this).addClass('selected');
+	    $(this).css('background-position', '0 -200px');
+	    $('.descriptionBox').css('display', 'block');
 	    if($(this).attr('id') == "harry"){
 	    	$('.descriptionBox').html("<p>The only one who actually does any work.</p>");
 	    }
@@ -11,7 +14,7 @@ $(document).ready(function(){
 	    	$('.descriptionBox').html("<p>A tard who knows how to use pliers.</p>");
 	    }
 	    else if($(this).attr('id') == "eric"){
-	    	$('.descriptionBox').html('<p>An even bigger tard.</p>');
+	    	$('.descriptionBox').html("<p>An even bigger tard.</p>");
 	    }
 	});
 });
