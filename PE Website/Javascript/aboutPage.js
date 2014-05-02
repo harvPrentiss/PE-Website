00875ef449ebe0b7f9b15ceae95b157b5380e008
Harold Prentiss <harv.prentiss@gmail.com>
2014-05-02 19:03:56 -0400

    Style changes to bring in line with the theme

--- PE Website/Javascript/aboutPage.js
+++ PE Website/Javascript/aboutPage.js
@@ -9,12 +9,21 @@ $(document).ready(function(){
 	    $('.descriptionBox').css('display', 'block');
 	    if($(this).attr('id') == "harry"){
 	    	$('.descriptionBox').html("<p>The only one who actually does any work.</p>");
+	    	$('#H').addClass('backFrontColorSwap');
+	    	$('#A').removeClass('backFrontColorSwap');
+	    	$('#E').removeClass('backFrontColorSwap');
 	    }
 	    else if($(this).attr('id') == "alex"){
 	    	$('.descriptionBox').html("<p>A tard who knows how to use pliers.</p>");
+	    	$('#A').addClass('backFrontColorSwap');
+	    	$('#H').removeClass('backFrontColorSwap');
+	    	$('#E').removeClass('backFrontColorSwap');
 	    }
 	    else if($(this).attr('id') == "eric"){
 	    	$('.descriptionBox').html("<p>An even bigger tard.</p>");
+	    	$('#E').addClass('backFrontColorSwap');
+	    	$('#A').removeClass('backFrontColorSwap');
+	    	$('#H').removeClass('backFrontColorSwap');
 	    }
 	});
 });
